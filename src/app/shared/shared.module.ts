6382ffd4ad2e96dd709763b2c383db6f1a122cef
thalysson <thalyssonwg.nascimento@gmail.com>
2022-05-29 10:41:59 -0300

    feat: created home component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { HomeModule } from '@components/home/home.module';
 
-const baseModules = [];
+const baseModules = [HomeModule];
 
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [baseModules],
+  exports: [baseModules],
   declarations: [],
 })
 export class SharedModule {}
